ad201b46f8ddd2b56172be1458d1b4f341f9f1f6
Hảo Nguyễn <nguyenphuochao123@gmail.com>
2023-10-25 23:00:49 +0700

    m

---
+++ godashop/resources/views/product.blade.php
@@ -0,0 +1,30 @@
+<div class="product-container">
+    <div class="image">
+        <img class="img-responsive" src="../images/{{ $pro->featured_image }}" alt="">
+    </div>
+    <div class="product-meta">
+        <h5 class="name">
+            <a class="product-name" href="chi-tiet-san-pham.html"
+                title="{{ $pro->name }}">{{ $pro->name }}</a>
+        </h5>
+        <div class="product-item-price">
+            @if ($pro->price != $pro->sale_price)
+                <span class="product-item-regular">{{ number_format($pro->price) }}₫</span>
+            @endif
+            <span class="product-item-discount">{{ number_format($pro->sale_price) }}₫</span>
+        </div>
+    </div>
+    <div class="button-product-action clearfix">
+        <div class="cart icon">
+            <a class="btn btn-outline-inverse buy" product-id="{{ $pro->id }}"
+                href="javascript:void(0)" title="Thêm vào giỏ">
+                Thêm vào giỏ <i class="fa fa-shopping-cart"></i>
+            </a>
+        </div>
+        <div class="quickview icon">
+            <a class="btn btn-outline-inverse" href="chi-tiet-san-pham.html" title="Xem nhanh">
+                Xem chi tiết <i class="fa fa-eye"></i>
+            </a>
+        </div>
+    </div>
+</div>
--- godashop/routes/web.php
+++ godashop/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Frontend\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Front-end
+Route::get('/', [HomeController::class, 'index']);
